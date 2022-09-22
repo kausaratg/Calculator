98bbe67834c0bbd4e403fcaa619c3b35f68a42a3
kassy <kausaratg@gmail.com>
2022-09-22 16:56:39 +0100

    added template and other basic calculation functions

--- basic_function/urls.py
+++ basic_function/urls.py
@@ -3,7 +3,11 @@ from basic_function import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add", views.add, name="add")
+    path("add", views.add, name="add"),
+    path("subtract", views.subract, name="subtract"),
+    path("multiply", views.multiply, name="multiply"),
+    path("division", views.division, name="division"),
+
 ]
 
-
+                
--- basic_function/views.py
+++ basic_function/views.py
@@ -6,12 +6,56 @@ def index(request):
     return render(request, "index.html")
 
 def add(request):
-    num1 = request.POST['num1']
-    num2 = request.POST['num2']
-    if num1.isdigit() and num2.isdigit():
-        res = int(num1) + int(num2)
-        return render(request, "result.html", {"result":res})
+    if request.method == 'POST':
+        num1 = request.POST['num1']
+        num2 = request.POST['num2']
+        if num1.digit() and num2.isdigit():
+            res = int(num1) + int(num2)
+            return render(request, "add.html", {"res":res})
+        else:
+            res = "Invalid input"
+            return render(request, "add.html", {"res":res})
     else:
-        res = "Invalid input. Make sure you input only number"
-        return render(request, "result.html", {"result":res})
+        return render(request, "add.html")
+
     
+def subract(request):
+    if request.method == 'POST':
+        num1 = request.POST['num1']
+        num2 = request.POST['num2']
+        if num1.isdigit() and num2.isdigit():
+            res = int(num1) - int(num2)
+            return render(request, "subtraction.html", {'res':res})
+        else:
+            res = "Invalid input"
+            return render(request, "subtraction.html", {"res":res})
+    else:
+        return render(request, "subtraction.html")
+
+
+def multiply(request):
+    if request.method == 'POST':
+        num1 = request.POST['num1']
+        num2 = request.POST['num2']
+        if num1.isdigit() and num2.isdigit():
+            res = int(num1) * int(num2)
+            return render(request, "multiplication.html", {'res':res})
+        else:
+            res = "Invalid input"
+            return render(request, "multiplication.html", {"res":res})
+    else:
+        return render(request, "multiplication.html")
+
+
+def division(request):
+    if request.method == 'POST':
+        num1 = request.POST['num1']
+        num2 = request.POST['num2']
+        if num1.isdigit() and num2.digit():
+            res = int(num1) / int(num2)
+            return render(request, "division.html", {'res':res})
+        else:
+            res = "Invalid input."
+            return render(request, "division.html", {"res":res})
+    else:
+        return render(request, "division.html")
